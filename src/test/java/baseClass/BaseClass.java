0f07b0519e3f81c5df0e59449eeb77169e3fb498
mohanasundaramp <mohanasundaramp@localhost>
2026-06-10 21:27:21 +0530

    DC

---
+++ src/test/java/baseClass/BaseClass.java
@@ -0,0 +1,14 @@
+package baseClass;
+
+import org.openqa.selenium.WebDriver;
+
+public class BaseClass {
+	
+	WebDriver driver;
+	
+	BaseClass(WebDriver driver)
+	{
+		this.driver = driver;
+	}
+
+}
