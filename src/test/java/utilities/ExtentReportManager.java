837f489f18c24cc9cdb900968c8229d8e5e9ae99
mohanasundaramp <mohanasundaramp@localhost>
2026-06-15 21:41:28 +0530

    DC

--- src/test/java/utilities/ExtentReportManager.java
+++ src/test/java/utilities/ExtentReportManager.java
@@ -1,5 +1,18 @@
 package utilities;
 
-public class ExtentReportManager {
+import org.testng.ITestListener;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReportManager implements ITestListener 
+{
+	// Implement methods of ITestListener to generate Extent Reports
+	
+	public ExtentSparkReporter extentReporter;
+	public ExtentReports extentReport;
+	public ExtentTest extentTest;	
+	
 
 }
