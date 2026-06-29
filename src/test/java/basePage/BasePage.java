2b6e298c155576e8dba73461ac44d008d3a6236b
mohanasundaramp <mohanasundaramp@localhost>
2026-06-29 13:56:41 +0530

    DC

--- src/test/java/basePage/BasePage.java
+++ src/test/java/basePage/BasePage.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
